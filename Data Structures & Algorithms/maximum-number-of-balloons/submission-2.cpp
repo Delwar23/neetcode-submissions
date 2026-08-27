479fd0121d0c6b9dfbc919c83754835020aa510c
Delwar23 <47058472+Delwar23@users.noreply.github.com>
2026-08-27 23:41:27 +0600

    Add: maximum-number-of-balloons - submission-2

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+   public:
+    int maxNumberOfBalloons(string text) {
+        map<char, int> mp;
+
+        for (int i = 0; i < text.size(); i++) {
+            mp[text[i]]++;
+        }
+        string tep = "balloon";
+        if (mp['l'] < 2 || mp['o'] < 2) return 0;
+        int res = INT_MAX;
+        for (int i = 0; i < tep.size(); i++) {
+            res = min(mp[tep[i]], res);
+        }
+
+        res = min(res, (mp['l']/2));
+        res = min(res, (mp['o']/2));
+        return res;
+    }
+};
